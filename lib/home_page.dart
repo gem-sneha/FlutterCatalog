01a6fffb55cf5048ff2c409a6f2b75a5ca7fd2fb
gem-sneha <sneha.goel@Geminisolutions.com>
2023-04-21 17:45:56 +0530

    day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,25 @@
+import "package:flutter/material.dart";
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: MaterialApp(
+          home: Material(
+          child: Center(
+          child: Container(
+        child: Text("Welcome"),
+      ),
+      ),
+    ),
+    ),
+    drawer: Drawer(),
+    );
+    
+  }
+}
