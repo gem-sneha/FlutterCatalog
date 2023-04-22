a31068368d758c46341acee59ef8afcd5dc22318
gem-sneha <sneha.goel@Geminisolutions.com>
2023-04-23 01:15:50 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/home_page.dart';
+import 'package:my_app/pages/home_page.dart';
+import 'package:my_app/pages/login_page.dart';
 
 void main() {
   runApp( MyApp());
@@ -11,7 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: HomePage(),
+    
+    routes: {
+      "/": (context) => HomePage(),
+       "/home": (context) => LoginPage(),
+      "/login" : (context) =>LoginPage(),
+    },
     );
     
   }  
